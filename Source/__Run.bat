8da8469eeb41047b05ddbfe38af396daa771a389
menrot <morldy@hotmail.com>
2019-05-29 17:46:01 +0300

    Mark old mode as obsolete

--- Source/__Run.bat
+++ Source/__Run.bat
@@ -1,5 +1,6 @@
-LeumiMail.py menrot -P mr8833175  1>LeumiMail.log 2>&1
-type LeumiMail.log
-pause
-explorer.exe "D:\Users\Menashe\G Drive\MyProjects\LeumiMail\temp\attachments"
-explorer.exe "D:\Users\Menashe\G Drive\0 Personal G\Bank Leumi"
+type "** THis file is obsolete"
+rem LeumiMail.py menrot -P password  1>LeumiMail.log 2>&1
+rem type LeumiMail.log
+rem pause
+rem explorer.exe "D:\Users\Menashe\mG Drive\MyProjects\LeumiMail\temp\attachments"
+rem explorer.exe "D:\Users\Menashe\mG Drive\0 Personal G\Bank Leumi"
