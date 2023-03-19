caf044b94fdada6a95815ee7c705725257983ad9
menrot <menrot@users.noreply.github.com>
2023-03-19 16:52:42 +0200

    Clean files

--- Source/__Run downloaded_B.bat
+++ Source/__Run downloaded_B.bat
@@ -1,9 +1,11 @@
 chcp 1255
 set "gdr=G:\My Drive"
-set "dwn=%gdr%\winsysfolders\downloads"
+set "dwn=%gdr%\MrProjects\LeumiMail\production\temp\zips"
+call ..\venv\Scripts\activate.bat
+@echo on
 python LeumiMail.py -Z "%dwn%" -B Leumi 1>LeumiMailProcess.log 2>&1
 type LeumiMailProcess.log
-echo ***** Ctrl^C if want to abort or SPACE to move files to fnal destination
+echo ***** Ctrl^C if want to abort or SPACE to move files to final destination
 pause
 python movefiles.py ListOfAccounts.csv -D="%gdr%" 1>LeumiMailMove.log 2>&1
 type LeumiMailMove.log
